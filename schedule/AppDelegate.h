7e4fc0878948b16557a1bc86cc50948dadd58c5b
Tom Schoffelen <tomttb2010@gmail.com>
2015-04-28 00:23:58 +0200

    Initial Commit

---
+++ schedule/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  schedule
+//
+//  Created by Thomas Schoffelen on 28/04/15.
+//  Copyright (c) 2015 Scholica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ schedule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  schedule
+//
+//  Created by Thomas Schoffelen on 28/04/15.
+//  Copyright (c) 2015 Scholica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
